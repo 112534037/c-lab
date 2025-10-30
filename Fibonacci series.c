a8145f86f1b1676833e590145436513652bf842c
112534037 <rajalakshmisathya2008@gmail.com>
2025-10-29 22:54:15 -0700

    Add files via upload

---
+++ Fibonacci series.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long first = 0, second = 1, next;
+
+    // Get number of terms from user
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; ++i) {
+        printf("%lld ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("\n");
+    return 0;
+}
+
